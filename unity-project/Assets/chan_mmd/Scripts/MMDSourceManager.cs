40bd31414a3f55e136a4775c8e1046594f2bc9b7
Solomon Li <solomon@unity3d.com>
2017-08-20 11:28:12 +0800

    play different sources

---
+++ unity-project/Assets/chan_mmd/Scripts/MMDSourceManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MMDSourceManager : MonoBehaviour {
+
+	public RuntimeAnimatorController JiLeJingTu;
+	public RuntimeAnimatorController QianBenYing;
+	public Animator PlayerAnimator;
+
+	public AudioClip JiLeJingTuClip;
+	public AudioClip QianBenYingClip;
+	public AudioSource AudioPlayer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void PlayMMD() {
+//		PlayerAnimator.Play ("");
+		AudioPlayer.Play ();
+	}
+
+	public void PlayJilejingtu() {
+		SetJiLeJingTu ();
+		PlayMMD ();
+	}
+
+	public void PlayQianbenying() {
+		SetQianBenYing ();
+		PlayMMD ();
+	}
+
+	private void SetJiLeJingTu() {
+		PlayerAnimator.runtimeAnimatorController = JiLeJingTu;
+		AudioPlayer.clip = JiLeJingTuClip;
+	}
+
+	private void SetQianBenYing() {
+		PlayerAnimator.runtimeAnimatorController = QianBenYing;
+		AudioPlayer.clip = QianBenYingClip;
+	}
+}
